c29e17ce781ae1096a2c4caeda16bff20b9c3b1f
barboza-sensei <fernandezpabloandres@gmail.com>
2023-06-22 13:24:05 -0300

    agregada act4

---
+++ practico2/operadoresLogicos.js
@@ -0,0 +1,14 @@
+const readlineSync = require('readline-sync');
+let numero = readlineSync.question("Ingrese un numero entero: ");
+
+numero = Number(numero);
+
+if (numero > 0 && numero % 2 === 0) {
+  console.log("El numero es positivo y par");
+} else if (numero > 0 && numero % 2 !== 0) {
+  console.log("El numero es positivo e impar");
+} else if (numero < 0) {
+  console.log("El numero es negativo");
+} else if (numero === 0) {
+  console.log("El numero es cero");
+}
