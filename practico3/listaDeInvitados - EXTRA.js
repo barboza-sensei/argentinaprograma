89838500fe4bfc9eef2b1878910821d4037aa542
barboza-sensei <fernandezpabloandres@gmail.com>
2023-06-27 15:15:49 -0300

    agregada act extra y P3 finalizado

---
+++ practico3/listaDeInvitados - EXTRA.js
@@ -0,0 +1,24 @@
+let invitados = ["Jose", "Maria", "Juan", "Andres", "Lionel", "Sofia", "Leandro", "Emilia"];
+let admitidos = [];
+let rechazados = [];
+
+for (let i = 0; i < invitados.length; i++) {
+  if (invitados[i] === "Jose" || invitados[i] === "Sofia") {
+    rechazados.push(invitados[i]);
+  } else {
+    admitidos.push(invitados[i]);
+  }
+}
+
+admitidos.sort();
+rechazados.sort();
+
+console.log("La lista ordenada de invitados admitidos es:");
+for (let i = 0; i < admitidos.length; i++) {
+  console.log('- ' + admitidos[i]);
+}
+
+console.log("La lista ordenada de invitados rechazados es:");
+for (let i = 0; i < rechazados.length; i++) {
+  console.log('- ' + rechazados[i]);
+}
